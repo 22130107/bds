7463f3290132560340fe7a82ddac13a137a0730c
22130107 <22130107@st.hcmuaf.edu.vn>
2026-05-01 14:27:17 +0700

    hhhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ export const metadata: Metadata = {
 };
 
 export const viewport = {
-  width: "990",
+  width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
